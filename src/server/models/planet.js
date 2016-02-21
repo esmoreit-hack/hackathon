ddb768b50bf515ff64ad6d4725a3f5de980467bb
Massimiliano <massimilianoscifo@gmail.com>
2016-02-21 09:26:32 +0100

    Mapgeneretor now return all the universe space

--- src/server/models/planet.js
+++ src/server/models/planet.js
@@ -15,42 +15,52 @@ const PlanetSchema = new Schema({
 PlanetSchema.plugin(require('mongoose-lifecycle'));
 
 PlanetSchema.methods.saveAllPlanets = (instance) => {
-
+	let count = 0;
 	map.universes.forEach( (universe) => {
 		universe.galaxies.forEach( (galaxy, index) => {
 			galaxy.planets.forEach( (planet) => {
-				let populated = false;
-				if(!populated) {
-					var vector3 = [];
-					if(planet.position){
-						planet.position.forEach((coord) => {
-							if(vector3.length < 3){
-								if(coord >= 0){
-									vector3.push(coord + index);
-								}else if(coord < 0){
-									vector3.push(coord - index);
-								}
-
-								if(vector3.length == 3) {
-									let PlanetModel = new Planet;
-									PlanetModel.Resources = ['1233', '32324', '325554'];
-									PlanetModel.GameInstance = instance;
-									PlanetModel.Position = vector3;
-									console.log(vector3);
-									/*PlanetModel.save((err, newPlanet) => {
-										if(err) console.log(err);
-
-										console.log(newPlanet);
-									});*/
-								}
+				var vector3 = [];
+				if(planet){
+					planet.forEach((coord) => {
+						var check = [];
+						console.log(vector3.length);
+						if(vector3.length == 3){
+							if(coord >= 0){
+								vector3.push(coord + index);
+							}else if(coord < 0){
+								vector3.push(coord - index);
 							}
-						});
-					}
-					populated = true;
+							check.push(vector3);
+
+							console.log(vector3);
+
+							if(vector3.length == 3) {
+								check.forEach( function(ck, index) {
+									if(vector3[0] == ck[0] && vector3[1] == ck[1] && vector3[2] == ck[2]) {
+										console.log('doble planet!');
+										return;
+									}
+								});
+							} else {
+								count++;
+								let PlanetModel = new Planet;
+								PlanetModel.Resources = ['1233', '32324', '325554'];
+								PlanetModel.GameInstance = instance;
+								PlanetModel.Position = vector3;
+								check = [];
+								/*PlanetModel.save((err, newPlanet) => {
+									if(err) console.log(err);
+
+									console.log(newPlanet);
+								});*/
+							}
+						}
+					});
 				}
 			});
 		});
 	});
+	console.log(count);
 };
 
 let Planet = Mongoose.model('planet', PlanetSchema);
